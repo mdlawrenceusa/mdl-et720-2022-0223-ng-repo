f5f0b25de8ae37f7cc5a1076da7d379adb330725
mdlawrenceusa <mdlawrence@me.com>
2022-02-24 16:30:17 -0500

    2 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,4 +1,4 @@
-export interface Product {
+export interface Stuf {
   id: number;
   name: string;
   price: number;
@@ -10,22 +10,27 @@ export const products = [
     id: 1,
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
   },
   {
     id: 2,
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
   },
   {
     id: 3,
     name: 'Phone Standard',
     price: 299,
-    description: ''
-  }
+    description: '',
+  },
+  {
+    id: 4,
+    name: 'Phone Super',
+    price: 1000,
+    description: 'Special equipment for ET-720 Students',
+  },
 ];
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
